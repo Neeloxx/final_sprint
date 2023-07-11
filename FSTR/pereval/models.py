07e9b44bb11147096d516d7c3073ae69cef2cbf5
Andrey <archackov.andrey@yandex.ru>
2023-07-11 19:42:15 +0300

    добавлены все модели и сериализаторы

--- FSTR/pereval/models.py
+++ FSTR/pereval/models.py
@@ -20,3 +20,52 @@ class Coords(models.Model):
     height = models.IntegerField(verbose_name='Height', null=True)
 
 
+class Level(models.Model):
+    winter = models.CharField(max_length=10, verbose_name='Winter', null=True, blank=True)
+    summer = models.CharField(max_length=10, verbose_name='Summer', null=True, blank=True)
+    autumn = models.CharField(max_length=10, verbose_name='Autumn', null=True, blank=True)
+    spring = models.CharField(max_length=10, verbose_name='Spring', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.winter} {self.summer} {self.autumn} {self.spring}'
+
+
+class Image(models.Model):
+    title = models.CharField(blank=True, null=True)
+    data_added = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return f'id: {self.pk}, title:{self.title} {self.image}'
+
+
+class Pereval(models.Model):
+
+    NEW = 'new'
+    PENDING = 'pending'
+    ACCEPTED = 'accepted'
+    REJECTED = 'rejected'
+
+    CHOICES_MODER = [
+        (NEW, 'new'),
+        (PENDING, 'pending'),
+        (ACCEPTED, 'accepted'),
+        (REJECTED, 'rejected'),
+    ]
+
+    beauty_title = models.CharField(max_length=30, null=False)
+    title = models.CharField(max_length=30, null=False)
+    other_titles = models.CharField(max_length=30, null=False)
+    connect = models.CharField(max_length=30, null=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    status = models.CharField(max_length=8, choices=CHOICES_MODER, default=NEW)
+    coords = models.ForeignKey('Coords', on_delete=models.CASCADE)
+    image = models.ForeignKey('Image', on_delete=models.CASCADE)
+    level = models.ForeignKey('Level', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.beauty_title} {self.title} {self.other_titles} id: {self.pk}'
--- FSTR/pereval/serializers.py
+++ FSTR/pereval/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from drf_writable_nested import WritableNestedModelSerializer
+from rest_framework.serializers import ValidationError
 from .models import *
 
 
@@ -12,3 +14,72 @@ class CoordsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coords
         fields = ['latitude', 'longitude', 'height']
+
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = ['winter', 'summer', 'autumn', 'spring']
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['title', 'image']
+
+
+class PerevalSerializer(WritableNestedModelSerializer,
+                        serializers.ModelSerializer):
+    user = UserSerializer()
+    coords = CoordsSerializer()
+    level = LevelSerializer()
+    image = ImageSerializer()
+
+    class Meta:
+        model = Pereval
+        exclude = 'status'
+
+    def validate(self, attrs):
+        user_data = attrs.get('user')
+        if not user_data:
+            raise ValidationError("User data is missing.")
+
+        if self.instance:
+            user = self.instance.user
+        else:
+            try:
+                user = User.objects.get(email=user_data.get('email'))  # .exists())
+            except User.DoesNotExist:
+                user = None
+
+        if user is not None:
+            if user.name != user_data.get('name') or \
+                    user.otc != user_data.get('otc') or user.fam != user_data.get('fam') or \
+                    user.phone != user_data.get('phone') or user.email != user_data.get('email'):
+                raise ValidationError("Information cannot be changed")
+
+        super().validate(attrs)
+
+        return attrs
+
+    def create(self, validated_data):
+
+        user_data = validated_data.pop('user')
+
+        coords_data = validated_data.pop('coords')
+        coords = Coords.objects.create(**coords_data)
+
+        level_data = validated_data.pop('level')
+        level = Level.objects.create(**level_data)
+
+        images_data = validated_data.pop('image')
+        image = Image.objects.create(**images_data)
+
+        if Pereval.objects.filter(user__email=user_data.get('email')).exists():
+            user = User.objects.get(email=user_data.get('email')).pk
+            pereval = Pereval.objects.create(**validated_data, user=User.objects.get(email=user_data.get('email')),
+                                             level=level, coords=coords, image=image)
+        else:
+            user = User.objects.create(**user_data)
+            pereval = Pereval.objects.create(**validated_data, user=user, level=level, coords=coords, image=image)
+        return pereval
